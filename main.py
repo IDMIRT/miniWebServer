38f745cf5edf93299e2fb58657c90ab81dd48c41
idmirt <idmirt@gmail.com>
2023-03-26 00:48:16 +1100

    add not found and hdrs line

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ class WebHandler(BaseRequestHandler):
         # self.request.send(HDRS.dataencode('utf-8')+content.encode('utf-8'))
         self.request.send(return_data)
 
-def hdrs(status_line):
+def hdrs(status_line:str):
     http = 'HTTP/1.1'
-    status_query =' '.join([status_line,'OK'])
-    return ''.join([http,status_query,'\r\n'])
-     
-          
-          
+    if status_line == '200':
+        status_query =' '.join([status_line,'OK'])
+    else:
+        status_query =' '.join([status_line,'Not Found'])
+
+    return ' '.join([http,status_query,'\r\n'])         
           
 
 
@@ -36,26 +37,29 @@ def return_page_query(data):
      return page
 
 def ReturnPageWithHTTPRequest(data):
-    HDRS = 'HTTP/1.1 200 OK \r\nContent-Type: text\html; charset=utf-8\r\n\r\n'
+    content = None
+    content_type = 'Content-Type: text\html; charset=utf-8\r\n\r\n'
+    HDRS = None#hdrs('200')+content_type#'HTTP/1.1 200 OK \r\nContent-Type: text\html; charset=utf-8\r\n\r\n'
     page = return_page_query(data)
     if page == '/':
+        HDRS = hdrs('200')+content_type
         with open('pages/index.html','r+', encoding='utf-8') as new_page:
                 content = new_page.read()
+    else:
+         HDRS = hdrs('404')+content_type
+         with open('pages/notfound.html','r+', encoding='utf-8') as new_page:
+                content = new_page.read()
+        
     return HDRS.encode('utf-8')+content.encode('utf-8')
 
 
-# def start_server():
-#     serv_web = ThreadingTCPServer(('',10500) ,WebHandler)
-#     serv_web.serve_forever()
 
 if __name__ == '__main__':
-    serv_web = ThreadingTCPServer(('',10500) ,WebHandler)
-    serv_web.serve_forever()
+    try:
+        serv_web = ThreadingTCPServer(('',10500) ,WebHandler)
+        serv_web.serve_forever()
+    except KeyboardInterrupt:
+        serv_web.server_close()
 
-    # with serv_web:
-
-    #     server_thread = Thread
-
-    # start_server()
-    # serv_web =  ThreadingTCPServer(('',10500) ,WebHandler)
+    
     
